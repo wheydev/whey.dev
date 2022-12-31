ff013948d799e2a456149708bb9ef54713ff4498
Eder Christian <ederchristian92@gmail.com>
2022-12-31 16:01:14 +0000

    [wip] Add initial structure

---
+++ src/config.ts
@@ -0,0 +1,99 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://whey.dev",
+  author: "Eder Christian",
+  desc: "Construa a base de programação para te deixar mais forte.",
+  title: "WheyDev",
+  ogImage: "wheydev-og.png",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/wheydev",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: false,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: false,
+  },
+  {
+    name: "Mail",
+    href: "mailto:yourmail@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
+  },
+  {
+    name: "Twitter",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: false,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/wheydev",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+];
---
+++ src/pages/sobre.md
@@ -0,0 +1,24 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "Sobre"
+---
+
+WheyDev é um site criado com o objetivo de ajudar pessoas que trabalham com programação, ou entusiastas, a ficarem mais fortes, construindo uma base de tecnologia mais sólida.
+
+## Propósito
+
+Ajudar pessoas que trabalham com tecnologia (ou entusiastas) a ficarem fortes e resilientes frente aos desafios, num mundo de constante mudança.
+
+## Visão
+
+Construir com as pessoas uma base — não somente técnica, de programação, mas também de valores e da vida — para podermos fazer o melhor trabalho possível, ocasionando uma mudança positiva no nosso entorno.
+
+## Valores
+
+- Disciplina
+- Força
+- Resiliência
+- Determinação
+- Comunidade
+- Conhecimento
+- Curiosidade
