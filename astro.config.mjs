cd30b2f34d17ecc28bf3385aa39930fd23ddcf5a
Eder Christian <ederchristian92@gmail.com>
2022-12-31 16:56:40 +0000

    Fix Partytown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
