1652a352a8749c17dbfbd34f889fd0300eeaff2e
Eder Christian <ederchristian92@gmail.com>
2024-09-23 22:44:09 -0300

    Add Author text

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, modDatetime, description } = frontmatter;
+  const { title, pubDatetime, modDatetime, description, author } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
@@ -29,6 +29,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         )}
       </a>
       <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} />
+      <span className="text-sm opacity-80">Author: {author}</span>
       <p>{description}</p>
     </li>
   );
