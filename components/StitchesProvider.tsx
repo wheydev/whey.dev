6efa40df018eb032644a45540a94e2cb478a370e
Eder Christian <ederchristian92@gmail.com>
2025-09-09 23:42:51 -0300

    Remove CSS injection logic from StitchesProvider

--- components/StitchesProvider.tsx
+++ components/StitchesProvider.tsx
@@ -1,23 +1,11 @@
 'use client'
 
 import { useEffect } from 'react'
-import { getCssText, globalStyles } from '@/stitches.config'
+import { globalStyles } from '@/stitches.config'
 
 export default function StitchesProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     globalStyles()
-
-    const style = document.createElement('style')
-    style.id = 'stitches'
-    style.innerHTML = getCssText()
-    document.head.appendChild(style)
-    
-    return () => {
-      const existingStyle = document.getElementById('stitches')
-      if (existingStyle) {
-        existingStyle.remove()
-      }
-    }
   }, [])
 
   return <>{children}</>
