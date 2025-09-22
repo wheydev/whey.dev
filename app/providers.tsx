6f1c157de6fcc4ccc76f98e935eb77372b0afd20
Eder Christian <ederchristian92@gmail.com>
2025-09-21 21:36:14 -0300

    Initialize PostHog provider with analytics configuration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,31 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
 export function Providers({ children }: ProvidersProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
+      capture_pageview: false,
+      capture_pageleave: true,
+      capture_exceptions: true,
+      debug: process.env.NODE_ENV === 'development',
+    })
+  }, [])
+
   return (
     <SessionProvider>
-      {children}
+      <PostHogProvider client={posthog}>
+        {children}
+      </PostHogProvider>
     </SessionProvider>
   )
 }
